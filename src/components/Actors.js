e82496a5a6aa5f1d2518f1c7068b47743389401d
john <jmlovitsch@gmail.com>
2020-11-21 14:54:03 -0600

    commit tree e7a0cdc952fb73d8e12c5204f1dfc0cc8b1a883a

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,7 +4,8 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actors.map( d =>  <div><h2>{d.name}</h2> <ul>{d.movies.map( m => { return <li>{m}</li>})} </ul>  </div> )}
     </div>
   );
 };
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,10 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+    <h1>Movies Page</h1>
+        {movies.map(movie => {
+            return (<div><h3>Title: {movie.title}</h3><p>Run Time: {movie.time}</p> <ul> Movie Genres:{ movie.genres.map( g =>  {return  <li>{g}</li>}) } </ul> </div>)
+        })}
     </div>
   );
 };
